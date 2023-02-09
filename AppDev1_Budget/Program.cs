e6284db2114b707025cce98d20a21b10aae8c041
2183276 <2183276@PE325-17>
2023-02-09 12:14:03 -0500

    Add project files.

---
+++ AppDev1_Budget/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AppDev1_Budget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
